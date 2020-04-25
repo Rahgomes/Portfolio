203c4a18d4271cd7f8827cddbb860f3a41d85a65
Ramon Gomes <ramon_gds@hotmail.com>
2020-04-25 15:14:46 -0300

    Configurando o módulo Clean em Dist

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,14 @@
 const gulp = require('gulp')
-const {series, parallel} = require('gulp')
+const { series, parallel } = require('gulp')
 
-const pages = require('./src/gulpTasks/pages')
+const {pages,cleanDist} = require('./src/gulpTasks/pages')
 const { styles, stylesBootstrap, stylesFontAwesome } = require('./src/gulpTasks/styles')
 const { scripts, scriptsjQuery, scriptsBootstrap } = require('./src/gulpTasks/scripts')
 const images = require('./src/gulpTasks/images')
 const { server, watchSass } = require('./src/gulpTasks/server')
 
 module.exports.default = series(
+        series(cleanDist),
         parallel(
             series(pages, styles, stylesBootstrap, stylesFontAwesome),
             series(scriptsjQuery, scriptsBootstrap, scripts),
--- src/gulpTasks/pages.js
+++ src/gulpTasks/pages.js
@@ -1,14 +1,24 @@
 const gulp = require('gulp')
 const htmlmin = require('gulp-htmlmin')
+const clean = require('gulp-clean')
+
+function cleanDist() {
+
+    return gulp.src('dist')
+                .pipe(clean())
+}
 
 function pages() {
 
     return gulp.src('src/**/*.html')
                 .pipe(htmlmin({collapseWhitespace: true, removeComments: true}))
                 .pipe(gulp.dest('dist'))
-                // .pipe(gulp.dest('./'))
 }
 
 gulp.task('pages', pages)
+gulp.task('cleanDist', cleanDist)
 
-module.exports = pages
+module.exports = {
+    pages,
+    cleanDist
+}
