50f19d93dc179359dd723f0bad40d0062bb33f5f
Ramon Gomes <ramon_gds@hotmail.com>
2024-06-19 09:14:06 -0300

    Removendo sentry do projeto para ajuste de deploy estático

--- app/global-error.jsx
+++ app/global-error.jsx
@@ -5,10 +5,6 @@ import Error from "next/error";
 import { useEffect } from "react";
 
 export default function GlobalError({ error }) {
-  useEffect(() => {
-    Sentry.captureException(error);
-  }, [error]);
-
   return (
     <html>
       <body>
